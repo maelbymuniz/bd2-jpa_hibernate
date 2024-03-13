59c893b66b9052019806046225110417ec391834
Maelby Muniz <maelbymuniz@gmail.com>
2024-03-13 11:32:42 -0300

    Feat: Configuração do application.properties

---
+++ src/main/java/br/edu/ifpb/bd2/jpahibernate/JpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.bd2.jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ifpb/bd2/jpahibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.bd2.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
