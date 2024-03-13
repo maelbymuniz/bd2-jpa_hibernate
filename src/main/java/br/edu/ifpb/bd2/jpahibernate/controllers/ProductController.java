7b9be671147fe7fbf0c8ecd5089c145fcf9b47f8
Maelby Muniz <maelbymuniz@gmail.com>
2024-03-13 12:00:39 -0300

    Feat: ProductController created

---
+++ src/main/java/br/edu/ifpb/bd2/jpahibernate/controllers/ProductController.java
@@ -0,0 +1,30 @@
+package br.edu.ifpb.bd2.jpahibernate.controllers;
+
+import br.edu.ifpb.bd2.jpahibernate.dtos.ProductDTO;
+import br.edu.ifpb.bd2.jpahibernate.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> create(ProductDTO productDTO) {
+        Optional<ProductDTO> response = service.create(productDTO);
+
+        if (response.isPresent()) {
+            return new ResponseEntity<>(response.get(), HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+}
